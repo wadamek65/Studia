8f936f3a06d148dc6fffb3d0754f8fa49b9cb8f8
Unknown <wadamek65@gmail.com>
2018-04-25 16:35:30 +0200

    android project aggregates almost finished

--- ANDROID/project/app/src/main/java/com/example/wojta/project/AggregatesFragment.java
+++ ANDROID/project/app/src/main/java/com/example/wojta/project/AggregatesFragment.java
@@ -5,10 +5,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AggregatesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_aggregates, container, false);
+        View view =  inflater.inflate(R.layout.fragment_aggregates, container, false);
+        final Button fetchButton = view.findViewById(R.id.fetch_button);
+        final Button fillButton = view.findViewById(R.id.fill_button);
+
+        final EditText dateStartInput = view.findViewById(R.id.date_start_input);
+        final EditText dateEndInput = view.findViewById(R.id.date_end_input);
+        final EditText cordIdInput = view.findViewById(R.id.cord_id_input);
+        final EditText acronymInput = view.findViewById(R.id.acronym_input);
+        final EditText kpiInput = view.findViewById(R.id.kpi_input);
+        final EditText binsInput = view.findViewById(R.id.bins_input);
+
+        fillButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dateStartInput.setText("2016-06-01");
+                dateEndInput.setText("2018-01-01");
+                cordIdInput.setText("Skuntank");
+                acronymInput.setText("dilfihess");
+                kpiInput.setText("SGSN_2012");
+            }
+        });
+
+        return view;
     }
 }
--- ANDROID/project/app/src/main/java/com/example/wojta/project/HomeFragment.java
+++ ANDROID/project/app/src/main/java/com/example/wojta/project/HomeFragment.java
@@ -1,14 +1,31 @@
 package com.example.wojta.project;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
+
+    //Button project_button;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        final Button project_button = (Button) view.findViewById(R.id.project_link);
+
+        project_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.healthiness-of-data.ovh"));
+                startActivity(browserIntent);
+            }
+        });
+        return view;
     }
 }
